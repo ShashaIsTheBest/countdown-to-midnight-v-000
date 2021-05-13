bf598ceb20cb675f2a59b89aeacde6d37229fd4d
Shasha Yan <shashayan@gmail.com>
2021-05-13 13:53:00 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,5 +1,6 @@
 #write your code here
 def countdown_with_sleep(seconds_to_midnight)
+  seconds_to_midnight = 10
   while seconds_to_midnight > 0
     puts "#{seconds_to_midnight} SECOND(S)!"
     seconds_to_midnight -= 1
