0cd31c5b671ad63082fcd179119fa95bc2bfce85
Shasha Yan <shashayan@gmail.com>
2021-05-13 13:58:59 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -7,7 +7,7 @@ def countdown(seconds_to_midnight)
 end
 
 def countdown_with_sleep(seconds_to_midnight)
-  seconds_to_midnight = 1
+  sleep(1)
   seconds_to_midnight -= 1
   end
   "HAPPY NEW YEAR!"
