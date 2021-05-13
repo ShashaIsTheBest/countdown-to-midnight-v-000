a925936b9edfdf85f9a6f21577b56fc3f567a7af
Shasha Yan <shashayan@gmail.com>
2021-05-13 13:50:21 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,8 +1,8 @@
 #write your code here
 def countdown(seconds_to_midnight)
-while seconds_to_midnight < 0
-  puts "#{number} SECOND(S)!"
-  seconds_to_midnight -= 1
-end 
+  while seconds_to_midnight > 0
+    puts "#{number} SECOND(S)!"
+    seconds_to_midnight -= 1
+  end
   puts "HAPPY NEW YEAR!"
 end
